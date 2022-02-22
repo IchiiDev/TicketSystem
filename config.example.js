4e17d4add285211f54cffec63aa352220c07d20b
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2022-02-22 10:48:53 +0100

    Adding config files

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    API_PORT: 3000,
+    OP_FRONT_PORT: 3001,
+    CLIENT_FRONT_PORT: 3002,
+    UPDATE_SCREEN_PORT: 3003,
+    WSS_PORT: 3004,
+    DEV_MODE: true, 
+}
---
+++ credentials.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DB_ACCOUNT: {
+        USERNAME: "",
+        PASSWORD: "",
+        HOST: "",
+        DATABASE: ""
+    }
+}
