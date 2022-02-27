0c6798ea7e05e6fbebe963c857c575868f133545
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2022-02-27 13:51:46 +0100

    Updating loggers

--- src/core/logging.js
+++ src/core/logging.js
@@ -2,8 +2,24 @@ const log4js = require("log4js");
 
 log4js.configure({
     appenders: { 
-        out: { type: "stdout"}, 
-        process: { type: "file", filename: "logs/process.log", maxLogSize: 10485760, backups: 3, keepFileExt: true },
+        out: { 
+            type: "stdout", 
+            layout: { 
+                type: "pattern", 
+                pattern: "%[[%d{hh:mm:ss}] [%p] %c -%] %m"
+            } 
+        }, 
+        process: { 
+            type: "file", 
+            filename: "logs/process.log", 
+            maxLogSize: 10485760, 
+            backups: 3, 
+            keepFileExt: true, 
+            layout: { 
+                type: "pattern", 
+                pattern: "[%d{yyyy/MM/dd | hh:mm:ss}] [%p] %c - %m" 
+            } 
+        },
     },
     categories: { 
         default: { appenders: ["process", "out"], level: "info" }, 
