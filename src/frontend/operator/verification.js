191e4b758dd8af93252a944d522a313b7c16481b
= <neo.elduayen@gmail.com>
2022-03-31 15:38:10 +0200

    modification fonction verifier permettant hash mdp

--- src/frontend/operator/verification.js
+++ src/frontend/operator/verification.js
@@ -1,9 +1,22 @@
 function verifier() {
-    if(document.formSaisie.username.value != "") {
-        document.formSaisie.submit();
+    if(document.getElementById("mdp").value != "" && document.getElementById("username").value != "") {
+        let username = document.getElementById("username").value
+        let mdp = document.getElementById("mdp").value
+        mdp = CryptoJS.MD5(mdp).toString()
+        console.log(`${username} ${mdp}`)
+        
+        var requete = {
+                method : 'POST',
+                headers: { 'Content-Type': "application/json" },
+                body: JSON.stringify({ "username" : username, "password": mdp})
+        }
+        fetch("http://localhost:3000/login", requete)
+            .then(req => req.json())
+            .then(json => {
+                console.log(json)
+            })
     }
     else{
         alert("Saissisez votre nom d'utilisateur");
     }
 }
-console.log(CryptoJS.MD5("test"))
