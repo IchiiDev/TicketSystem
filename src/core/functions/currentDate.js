c0d030d98738e2665e497128a1905573e1feba4b
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2022-04-05 22:16:35 +0200

    Adding date function

---
+++ src/core/functions/currentDate.js
@@ -0,0 +1,6 @@
+function currentDate() {
+    const date = new Date();
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`; 
+}
+
+module.exports = { currentDate };
