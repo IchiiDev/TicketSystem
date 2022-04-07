e58758de217db095f4013039f69111795ba633fc
Ethan Thierry <45918948+IchiiDev@users.noreply.github.com>
2022-04-07 15:14:01 +0200

    Fixing websocket

--- src/backend/websocket.js
+++ src/backend/websocket.js
@@ -61,40 +61,44 @@ function AuthEndpoint(data, socket) {
     if (data.token == "update_screen" && !server.update_screen) {
         logger.websocket.info("Client logged in as update_screen.");
         socket.isAuthed = true;
-        socket.sendMessage({ code: 200, message: "Authentified, successfully as update_screen" });
+        socket.sendMessage({ code: 200, message: "Authentified, successfully as update_screen", endpoint: "auth" });
         socket.permission_level = "update_screen";
         server.update_screen = socket;
     } else if (UserData[data.token] !== undefined) {
         logger.websocket.info(`Client logged in as ${UserData[data.token].username}`);
         socket.isAuthed = true;
-        socket.sendMessage({ code: 200, message: "Authentified, successfully as " + UserData[data.token].username });
+        socket.sendMessage({ code: 200, message: "Authentified, successfully as " + UserData[data.token].username, endpoint: "auth" });
         socket.permission_level = "operator";
         socket.username = UserData[data.token].username;
     } 
     else {
         logger.websocket.error("Client tried to log in with unknown token !");
-        socket.sendMessage({ code: 401, message: "Bad token."});
+        socket.sendMessage({ code: 401, message: "Bad token.", endpoint: "auth" });
     }
 }
 
 function nextTicket(data, socket) {
-    if (socket.permission_level !== "operator") return socket.sendMessage({ code: 401, message: "You don't have the permission to do that." });
+    if (socket.permission_level !== "operator") return socket.sendMessage({ code: 401, message: "You don't have the permission to do that.", endpoint: "next_ticket" });
 
     db.query("SELECT UID, display_name FROM tickets WHERE active = 1 LIMIT 1", (err, rows) => {
-        if (err) return logger.backend.error(err.message);
+        if (err) {
+            logger.backend.error(err.message);
+            socket.sendMessage({ code: 500, message: "Internal Server Error", error: err.message, endpoint: "next_ticket" });
+            return;
+        }
         if (rows.length == 0) {
-            socket.sendMessage({ code: 404, message: "No ticket found." });
-            server.update_screen.sendMessage({ code: 404, message: "No ticket found." });
+            socket.sendMessage({ code: 404, message: "No ticket found.", endpoint: "next_ticket" });
+            server.update_screen.sendMessage({ code: 404, message: "No ticket found.", endpoint: "next_ticket" });
             return;
         }
         db.query("UPDATE tickets SET active = 0, operator_name = ?, closed_date = ? WHERE UID = ?", [socket.username, currentDate(), rows[0].UID], (err) => {
             if (err) {
                 logger.backend.error(err.message);
-                socket.sendMessage({ code: 500, message: "Internal Server Error" });
+                socket.sendMessage({ code: 500, message: "Internal Server Error", error: err.message, endpoint: "next_ticket" });
                 return;
             }
-            socket.sendMessage({ code: 200, message: rows[0] });
-            server.update_screen.sendMessage({ code: 200, message: rows[1] });
+            socket.sendMessage({ code: 200, message: rows[0], endpoint: "next_ticket" });
+            server.update_screen.sendMessage({ code: 200, message: rows[1], endpoint: "next_ticket" });
         });
     });
 
